8c26efa3d0bd7fb08fa8196b8f6d7118cc99e775
Smit Shah <who828@gmail.com>
2013-06-04 15:49:40 +0530

    Keep track of the total spam and ham mails

--- lib/spam_filter.rb
+++ lib/spam_filter.rb
@@ -1,12 +1,15 @@
 class SpamFilter
-  attr_reader :words_hash
+  attr_reader :words_hash, :ham_count, :spam_count
 
   def initialize
     @words_hash = {}
+    @ham_count = 0
+    @spam_count = 0
   end
 
   def message(message, spam_status)
     words = message.split.map { |m| m.to_sym }
+    increment_counter(spam_status)
     words.each do |i|
       if @words_hash.key?([i,spam_status])
         @words_hash[[i,spam_status]] += 1
@@ -14,5 +17,12 @@ class SpamFilter
         @words_hash[[i, spam_status]] = 1
       end
     end
+  end
+
+  private
+
+  def increment_counter(status)
+    @ham_count += 1 if status == :good
+    @spam_count += 1 if status == :bad
   end
 end
