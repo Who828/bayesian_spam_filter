6af2b94c841a256146a1b519a190a00faa485f5b
Smit Shah <who828@gmail.com>
2013-06-04 23:49:20 +0530

    Finished an initial version bayesian spam filter

--- lib/spam_filter.rb
+++ lib/spam_filter.rb
@@ -9,7 +9,7 @@ class SpamFilter
   end
 
   def feed_message(message, spam_status)
-    words = message.split.map { |m| m.downcase.to_sym }
+    words = message.split(/\W+/).map { |m| m.downcase.to_sym }
     increment_counter(spam_status)
     words.each do |i|
       if @words_hash.key?([i,spam_status])
@@ -33,6 +33,27 @@ class SpamFilter
     word_probability = min(1.0, spam_word_frequency.to_f / spam_count)
     total_probability = word_probability + min(1.0, ham_word_frequency.to_f / ham_count)
     max(0.1, min(0.99, word_probability/total_probability))
+  end
+
+  def spam?(message)
+    spamicity(message) > 0.9
+  end
+
+  def spamicity(message)
+    words = message.split(/\W+/).map { |m| m.downcase.to_sym }
+    probs = intersting_words(words)
+    prods = probs.inject(:*)
+    prods / (prods + probs.map { |x| 1 - x }.inject(:*))
+  end
+
+  def intersting_words(words)
+    probs = words.map do |word|
+      unless @probability_hash.key?(word)
+        @probability_hash[word] = 0.4
+      end
+      probability_hash[word]
+    end
+    probs.compact.sort { |a, b| (b - 0.5).abs <=> (a - 0.5).abs }[0..14]
   end
 
   private
