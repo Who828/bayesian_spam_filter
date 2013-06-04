d05180c3428b0c54a12eb498bcc152cf316585b7
Smit Shah <who828@gmail.com>
2013-06-04 22:36:08 +0530

    Added ability to calculate probability after training the filter

--- lib/spam_filter.rb
+++ lib/spam_filter.rb
@@ -1,14 +1,15 @@
 class SpamFilter
-  attr_reader :words_hash, :ham_count, :spam_count
+  attr_reader :words_hash, :ham_count, :spam_count, :probability_hash
 
   def initialize
     @words_hash = {}
-    @ham_count = 0
-    @spam_count = 0
+    @probability_hash = {}
+    @ham_count = 1
+    @spam_count = 1
   end
 
-  def message(message, spam_status)
-    words = message.split.map { |m| m.to_sym }
+  def feed_message(message, spam_status)
+    words = message.split.map { |m| m.downcase.to_sym }
     increment_counter(spam_status)
     words.each do |i|
       if @words_hash.key?([i,spam_status])
@@ -19,10 +20,36 @@ class SpamFilter
     end
   end
 
+  def calculate_probabilities
+    @words_hash.keys.each do |word, _|
+      @probability_hash[word] = calculate_probability(word)
+    end
+  end
+
+  def calculate_probability(word)
+    ham_word_frequency = 2 * (words_hash[[word,:good]] || 0)
+    spam_word_frequency = words_hash[[word, :bad]] || 0
+    return if ham_word_frequency + spam_word_frequency < 5
+    word_probability = min(1.0, spam_word_frequency.to_f / spam_count)
+    total_probability = word_probability + min(1.0, ham_word_frequency.to_f / ham_count)
+    max(0.1, min(0.99, word_probability/total_probability))
+  end
+
   private
 
   def increment_counter(status)
     @ham_count += 1 if status == :good
     @spam_count += 1 if status == :bad
   end
+
+  def min(a, b)
+    return a if a < b
+    return b
+  end
+
+  def max(a, b)
+    return a if a > b
+    return b
+  end
+
 end
--- spec/spam_filter_spec.rb
+++ spec/spam_filter_spec.rb
@@ -4,26 +4,37 @@ describe SpamFilter do
   context "Messages processing" do
     it "creates a hashes of word based on the input" do
       spam_filter = SpamFilter.new
-      spam_filter.message("Life is great", :good)
-      spam_filter.words_hash[[:Life, :good]].should == 1
-      spam_filter.message("Life is great", :bad)
-      spam_filter.words_hash[[:Life, :bad]].should == 1
+      spam_filter.feed_message("Life is great", :good)
+      spam_filter.words_hash[[:life, :good]].should == 1
+      spam_filter.feed_message("Life is great", :bad)
+      spam_filter.words_hash[[:life, :bad]].should == 1
     end
 
     it "keeps track of the number of times a word has appeared in messages" do
       spam_filter = SpamFilter.new
-      spam_filter.message("Life is great", :good)
-      spam_filter.message("Life is great", :good)
-      spam_filter.words_hash[[:Life, :good]].should == 2
+      spam_filter.feed_message("Life is great", :good)
+      spam_filter.feed_message("Life is great", :good)
+      spam_filter.words_hash[[:life, :good]].should == 2
     end
 
     it "keep tracks of total ham and spam mails" do
       spam_filter = SpamFilter.new
-      spam_filter.message("Life is great", :bad)
-      spam_filter.message("Life is great", :bad)
-      spam_filter.message("Life is great", :good)
-      spam_filter.spam_count.should == 2
-      spam_filter.ham_count.should == 1
+      spam_filter.feed_message("Life is great", :bad)
+      spam_filter.feed_message("Life is great", :bad)
+      spam_filter.feed_message("Life is great", :good)
+      spam_filter.spam_count.should == 3
+      spam_filter.ham_count.should == 2
     end
+  end
+
+  it "calculates the correct probability" do
+    spam_filter = SpamFilter.new
+    spam_filter.feed_message("Get Viagra here without a prescription", :bad)
+    spam_filter.feed_message("Get Viagra here without a prescription", :bad)
+    spam_filter.feed_message("Get Viagra here without a prescription", :bad)
+    spam_filter.feed_message("Get Viagra here without a prescription", :bad)
+    spam_filter.feed_message("Get Viagra here without a prescription", :bad)
+    spam_filter.calculate_probabilities
+    spam_filter.probability_hash[:viagra].should == 0.99
   end
 end
