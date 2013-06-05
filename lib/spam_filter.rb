9a6b02c0b7c317c1be08033acf74c7923cc3bfa3
Smit Shah <who828@gmail.com>
2013-06-05 10:03:50 +0530

    Added comments at various places and renamed the variables

--- lib/spam_filter.rb
+++ lib/spam_filter.rb
@@ -2,7 +2,6 @@ require 'utility'
 
 class SpamFilter
   include Utility
-
   attr_reader :words_hash, :ham_count, :spam_count, :probability_hash
 
   def initialize
@@ -19,12 +18,16 @@ class SpamFilter
     end
   end
 
+  # Calculates probabilities for all of the words in word_hash
   def calculate_probabilities
     @words_hash.keys.each do |word, _|
       @probability_hash[word] = calculate_probability(word)
     end
   end
 
+  # Calculates probability using Baye's therom
+  # P(S|W) = P(W|S) * P(S) / P(W)
+  # P(W) = P(S) * P(W|S) + P(H) * P(W|H)
   def calculate_probability(word)
     ham_word_frequency = 2 * words_hash[[word,:good]]
     spam_word_frequency = words_hash[[word, :bad]]
@@ -38,16 +41,23 @@ class SpamFilter
     spamicity(message) > 0.9
   end
 
+  # Returns the combination of all of the interesting words in a message
   def spamicity(message)
     words = message.split(/\W+/).map { |m| m.downcase}
-    probs = intersting_words(words)
-    prods = probs.inject(:*)
-    prods / (prods + probs.map { |x| 1 - x }.inject(:*))
+    intersting_words_probability_list = intersting_words(words)
+    intersting_words_product = intersting_words_probability_list.inject(:*)
+    total_probability = intersting_words_product + intersting_words_probability_list.map { |x| 1 - x }.inject(:*)
+    intersting_words_product / total_probability
   end
 
+  # Returns the top 15 interesting words
+  # Intersting words meaan words with probability close to either 0.01 or 0.99
   def intersting_words(words)
-    probs = words.map { |word| probability_hash[word] }
-    probs.compact.sort { |a, b| (b - 0.5).abs <=> (a - 0.5).abs }[0..14]
+    words_probability_list = words.map { |word| probability_hash[word] }
+
+    # Sorting based on abs(probability - 0.5) ensures that probability close to 0.01 or 0.99
+    # will be at the top of the list, since we're sorting by descending order
+    words_probability_list.compact.sort { |a, b| (b - 0.5).abs <=> (a - 0.5).abs }[0..14]
   end
 
   private
